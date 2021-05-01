ca88d00912c71949e1489687711c93c2f81ce167
Jason Moore <jason@jasonmoore.xyz>
2021-05-01 23:25:08 +0100

    Added first bit of the program
    The program will now connect to the default X display and select
    substructure rediect and notify on the root window, and exiting if there
    is already a window manager running.

--- main.c
+++ main.c
@@ -1,9 +1,44 @@
 #include "header.h"
 
-char** g_argv;
+char** g_argv;    // Copy of argv to use in other functions
+Display* display; // The X display to connected to
+Window root;      // The root window of this display
+bool existsWM;    // Is there already a window manager running on this display
+
+int detectWM(Display* display, XErrorEvent* e);
 
 int main(int argc, char** argv) {
-    g_argv = argv;
+	g_argv = argv; // Preserve argv.
 
+	// Attempt to open the default display.
+	display = XOpenDisplay(NULL);
+	if (display == NULL)
+		die("Could not connect to X display.");
+
+	root = XDefaultRootWindow(display);
+
+	// Attempt to select substructure redirection on the root window.
+	// An error should only occur when there is already a window manager running, so the error
+	// handler will set existWM to true.
+	existsWM = false;
+	XSetErrorHandler(detectWM);
+	XSelectInput(display, root, SubstructureRedirectMask | SubstructureNotifyMask);
+	XSync(display, false);
+
+	// If we detected another window manager, we must stop.
+	if (existsWM) {
+		XCloseDisplay(display);
+		die("There is already a window manager running on this display.");
+	}
+
+	XCloseDisplay(display);
+
+	return 0;
+}
+
+// An error handler which sets the boolean existsWM if an error occurs while selecting substructure
+// redirection.
+int detectWM(Display* display, XErrorEvent* e) {
+	existsWM = true;
 	return 0;
 }
