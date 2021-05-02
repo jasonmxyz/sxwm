09ba00649a260fe27296ec0d9ec6e3d4d37f1c83
Jason Moore <jason@jasonmoore.xyz>
2021-05-02 01:58:48 +0100

    Added responses to ConfigureRequest and MapRequest
    The responses to ConfigureRequest and MapRequest events have been added.
    A function getClientFrame was added which returns the frame associated
    with a given window.

--- clientList.c
+++ clientList.c
@@ -7,3 +7,15 @@ void addClient(Client* client) {
 	(*client).next = clients;
 	clients = client;
 }
+
+// Get the frame window given the client window
+Window getClientFrame(Window window) {
+	Client* current = clients;
+	while (current != NULL) {
+		if (current->window == window) {
+			return current->frame;
+		}
+		current = current->next;
+	}
+	return (Window)NULL;
+}
--- clientList.h
+++ clientList.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <X11/Xlib.h>
+
 typedef struct Client Client;
 struct Client {
+	Window frame;
+	Window window;
 	Client* next;
 };
 
 void addClient(Client* client);
+Window getClientFrame(Window window);
