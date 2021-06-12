8d5d74006e9a3f3201621e90a4fa8080bda0252b
Jason Moore <jason@jasonmoore.xyz>
2021-06-12 06:00:21 +0100

    Tags are now drawn in the bar
    The bar now shows the tags available (from 1 to 9) and highlights the
    currently selected tag in a different colour. But it doesn't work
    properly yet because the currentTag variable is not updated in the fork
    from which the code runs.

--- src/bar.c
+++ src/bar.c
@@ -14,8 +14,10 @@ Window bar;
 Display* d;
 int s, width;
 GC gc;
+extern currentTag;
 
 void draw();
+void drawTags();
 
 void createBar() {
 	int result = fork();
@@ -52,9 +54,31 @@ void createBar() {
 void draw() {
 	const char *msg = "github.com/jasonmxyx/sxwm";
 
-	XSetForeground(d, gc, barSettings.bgColor2);
-	XFillRectangle(d, bar, gc, 0, 0, barSettings.height * 9, barSettings.height);
-
 	XSetForeground(d, gc, barSettings.fgColor1);
 	XDrawString(d, bar, gc, barSettings.height * 9 + 10, 20, msg, strlen(msg));
+
+	drawTags();
+}
+
+void drawTags() {
+	// Draw the main background rectangle
+	XSetForeground(d, gc, barSettings.bgColor2);
+	XFillRectangle(d, bar, gc, 0, 0, barSettings.height * 9, barSettings.height);
+	XSetForeground(d, gc, barSettings.fgColor2);
+
+	char tag[] = {'1', 0};
+
+	// Draw each of the tag numbers, and indicate the currently selected tag with another color.
+	for (int i = 1; i <= 9; i++, tag[0]++) {
+		// If this tag is slected, then draw another coloured rectangle
+		if (i == currentTag) {
+			XSetForeground(d, gc, barSettings.bgColor1);
+			XFillRectangle(d, bar, gc, barSettings.height * (i-1), 0, barSettings.height, barSettings.height);
+			XSetForeground(d, gc, barSettings.fgColor1);
+			XDrawString(d, bar, gc, barSettings.height * (i-1) + 10, 20, tag, 1);
+			XSetForeground(d, gc, barSettings.fgColor2);
+		}
+		// Otherwise, just draw the number
+		XDrawString(d, bar, gc, barSettings.height * (i-1) + 10, 20, tag, 1);
+	}
 }
